8effa567a37b39bf218b7d88c4613e65c4868855
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-03 18:50:02 -0500

    finished ArrayList and HashMap - 5 down, 7 to go

--- gettestie/src/test/java/rocks/zipcode/TestArrayList.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayList.java
@@ -1,4 +1,121 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestArrayList {
+
+
+    ArrayList<String> testArrayList;
+    Stack<String> stack;
+
+
+    @Before
+    public void setUp() throws Exception {
+        testArrayList = new ArrayList<>();
+        testArrayList.add("Cloud");
+        testArrayList.add("Tifa");
+        testArrayList.add("Yuffie");
+        testArrayList.add("Barret");
+        testArrayList.add("Aerith");
+        stack = new Stack<>();
+        stack.push("Hello world");
+        stack.push("Goodbye world");
+    }
+
+    @Test
+    //tests add
+    public void TestArrayList1() {
+        testArrayList.add("Red 13");
+        assertEquals(true, testArrayList.contains("Red 13"));
+    }
+
+    @Test
+    //tests add with index and get
+    public void TestArrayList2() {
+        testArrayList.add(1,"Red 13");
+        assertEquals("Red 13", testArrayList.get(1));
+    }
+
+    @Test
+    //tests addAll
+    public void TestArrayList3() {
+        testArrayList.addAll(stack);
+        assertEquals(true, testArrayList.contains("Hello world"));
+    }
+
+    @Test
+    //tests addAll with index
+    public void TestArrayList4() {
+        testArrayList.addAll(1, stack);
+        assertEquals("Goodbye world", testArrayList.get(2));
+    }
+
+    @Test
+    //tests clear
+    public void TestArrayList5() {
+        testArrayList.clear();
+        assertEquals(0, testArrayList.size());
+    }
+
+    @Test
+    //tests indexOf - should return the first index
+    public void TestArrayList6() {
+        testArrayList.add("Cloud");
+        assertEquals(0, testArrayList.indexOf("Cloud"));
+    }
+
+    @Test
+    //tests lastIndexOf - should return the last index
+    public void TestArrayList7() {
+        testArrayList.add("Cloud");
+        assertEquals(5, testArrayList.lastIndexOf("Cloud"));
+    }
+
+    @Test
+    //tests isEmpty
+    public void TestArrayList8() {
+        testArrayList.clear();
+        assertEquals(true, testArrayList.isEmpty());
+    }
+
+    @Test
+    //tests remove by index
+    public void TestArrayList9() {
+        testArrayList.remove(0);
+        assertEquals("Tifa", testArrayList.get(0));
+    }
+
+    @Test
+    //tests remove by object - should remove the first one only
+    public void TestArrayList10() {
+        testArrayList.add("Cloud");
+        testArrayList.remove("Cloud");
+        assertEquals(4, testArrayList.indexOf("Cloud"));
+    }
+
+    @Test
+    //tests sublist - basically substring for arrays
+    public void TestArrayList11() {
+        assertEquals(Arrays.asList("Cloud", "Tifa"), testArrayList.subList(0,2));    // returns the true pairing if you played the game right
+    }
+
+    @Test
+    //returns the correct response when challenged on the previous
+    public void TestArrayList12(){
+        testArrayList.add(4,"Goodbye");
+        assertEquals(Arrays.asList("Goodbye", "Aerith"), testArrayList.subList(4,testArrayList.size()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
 }
--- gettestie/src/test/java/rocks/zipcode/TestHashMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashMap.java
@@ -1,4 +1,84 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestHashMap {
+    //HASH MAPS ARE BEST WHEN YOU NEED A MAPPING, BUT THE ORDER DOESN'T MATTER
+
+    HashMap testHashMap;
+
+    @Before
+    public void setUp() throws Exception {
+        testHashMap = new HashMap();
+        testHashMap.put("Cloud", "Strife");
+        testHashMap.put("Tifa", "Lockheart");
+        testHashMap.put("Yuffie", "Kisaragi");
+        testHashMap.put("Aerith", "Gainsborough");
+        testHashMap.put("Barret", "Wallace");
+        testHashMap.put("Vincent", "Valentine");
+    }
+
+    @Test
+    //tests size
+    public void TestHashMap1() {
+        assertEquals(6, testHashMap.size());
+    }
+
+    @Test
+    //tests clear and isEmpty
+    public void TestHashMap2() {
+        testHashMap.clear();
+        assertEquals(true, testHashMap.isEmpty());
+    }
+
+    @Test
+    //tests containsKey
+    public void TestHashMap3() {
+        assertEquals(true, testHashMap.containsKey("Cloud"));
+    }
+
+    @Test
+    //tests containsValue
+    public void TestHashMap4() {
+        assertEquals(true, testHashMap.containsValue("Strife"));
+    }
+
+    @Test
+    //tests get
+    public void TestHashMap5() {
+        assertEquals("Strife", testHashMap.get("Cloud"));
+    }
+
+    @Test
+    //tests getOrDefault - returns a default value if the key is not found
+    public void TestHashMap6() {
+        String defaultValue = "no such user";
+        assertEquals("no such user", testHashMap.getOrDefault("Tom", defaultValue));
+    }
+
+    @Test
+    //tests putIfAbsent - this example will not change the map
+    public void TestHashMap7() {
+        testHashMap.putIfAbsent("Cloud", "Strife");
+        assertEquals(6, testHashMap.size());
+    }
+
+    @Test
+    //tests remove - removes the mapping to key "Cloud", but does not remove the actual key
+    public void TestHashMap8() {
+        testHashMap.remove("Cloud");
+        assertEquals(null, testHashMap.get("Cloud"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
 }
