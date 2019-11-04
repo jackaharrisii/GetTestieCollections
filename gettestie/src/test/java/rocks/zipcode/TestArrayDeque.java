3081df527902b007dde680ac542f8a9b11af5ed7
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-11-03 19:25:25 -0500

    added tests for LinkedList and ArrayDeque

--- gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
+++ gettestie/src/test/java/rocks/zipcode/TestArrayDeque.java
@@ -1,4 +1,107 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestArrayDeque {
+    //ADD OR REMOVE ITEMS TO EITHER THE FRONT OR BACK OF A LIST (BUT NOT THE MIDDLE...)
+    //TECHNICALLY, THERE IS NOTHING YOU CAN DO WITH AN ARRAYDEQUE THAT YOU CAN'T ALREADY DO WITH A LINKED LIST
+    //HOWEVER, IT IS FASTER TO PROCESS THAN A LINKED LIST OR STACK WHEN USING AS A QUEUE
+    //YOU CAN'T HAVE NULL ELEMENTS IN AN ARRAYDEQUE
+    //MAYBE USE THIS TO TRACK A REALLY LONG QUEUE WHERE THE END MIGHT LEAVE DUE TO FRUSTRATION, AS LONG AS NO ONE IN THE MIDDLE LEAVES TOO....
+    //https://docs.oracle.com/javase/7/docs/api/java/util/ArrayDeque.html
+
+    ArrayDeque<String> testArrayDeque;
+    Stack<String> stack;
+
+
+    @Before
+    public void setUp() throws Exception {
+        testArrayDeque = new ArrayDeque<>();
+        testArrayDeque.add("Cloud");
+        testArrayDeque.add("Tifa");
+        testArrayDeque.add("Yuffie");
+        testArrayDeque.add("Barret");
+        testArrayDeque.add("Aerith");
+        stack = new Stack<>();
+        stack.push("Hello world");
+        stack.push("Goodbye world");
+    }
+
+    @Test
+    //tests add
+    public void TestArrayList1() {
+        testArrayDeque.add("Red 13");
+        assertEquals(true, testArrayDeque.contains("Red 13"));
+    }
+
+    @Test
+    //tests addFirst and getFirst
+    public void TestArrayList2() {
+        testArrayDeque.addFirst("Red 13");
+        assertEquals("Red 13", testArrayDeque.getFirst());
+    }
+
+    @Test
+    //tests addAll
+    public void TestArrayList3() {
+        testArrayDeque.addAll(stack);
+        assertEquals(true, testArrayDeque.contains("Hello world"));
+    }
+//  LOL, not with an ArrayDeque
+//    @Test
+//    //tests addAll with index
+//    public void TestArrayList4() {
+//        testArrayDeque.addAll(1, stack);
+//        assertEquals("Goodbye world", testArrayDeque.get(2));
+//    }
+
+    @Test
+    //tests clear
+    public void TestArrayList5() {
+        testArrayDeque.clear();
+        assertEquals(0, testArrayDeque.size());
+    }
+
+    // Nope again - there are no iterable indexes here
+//    @Test
+//    //tests indexOf - should return the first index
+//    public void TestArrayList6() {
+//        testArrayDeque.add("Cloud");
+//        assertEquals(0, testArrayDeque.indexOf("Cloud"));
+//    }
+//
+//    @Test
+//    //tests lastIndexOf - should return the last index
+//    public void TestArrayList7() {
+//        testArrayDeque.add("Cloud");
+//        assertEquals(5, testArrayDeque.lastIndexOf("Cloud"));
+//    }
+
+    @Test
+    //tests isEmpty
+    public void TestArrayList8() {
+        testArrayDeque.clear();
+        assertEquals(true, testArrayDeque.isEmpty());
+    }
+
+    @Test
+    //tests pollLast - retrieves and removes the last item (aka - what needs to be done)
+    public void TestArrayList11() {
+        assertEquals("Aerith", testArrayDeque.pollLast());    // returns Aerith and removes her from the list
+        assertEquals(false, testArrayDeque.peekLast().equals("Aerith"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
 }
--- gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
+++ gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
@@ -1,4 +1,64 @@
 package rocks.zipcode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestLinkedList {
+    //KEEPS ALL ELEMENTS IN THE LIST IN THE ORDER RETURNED BY THE COLLECTION'S ITERATOR, I.E., THE ORDER THEY WERE PUT IN THE LIST
+
+
+    LinkedList testList;
+
+    @Before
+    public void setUp() throws Exception {
+        testList = new LinkedList();
+        testList.add("Alpha");
+        testList.add("Bravo");
+        testList.add("Charlie");
+        testList.add("Delta");
+        testList.add("Echo");
+    }
+
+    @Test
+    //tests add with an index - should be able to add anywhere in the list and move the others over
+    //also tests get
+    public void TestLinkedList1() {
+        testList.add(0, "Zulu");
+        assertEquals("Zulu", testList.get(0));
+        assertEquals("Echo", testList.get(5));
+    }
+
+    @Test
+    //tests addFirst - same action, different method
+    public void TestLinkedList2() {
+        testList.addFirst( "Zulu");
+        assertEquals("Zulu", testList.get(0));
+        assertEquals("Echo", testList.get(5));
+    }
+
+    @Test
+    //tests addLast - as advertised
+    public void TestLinkedList3() {
+        testList.addLast( "Zulu");
+        assertEquals("Zulu", testList.get(5));
+        assertEquals("Echo", testList.get(4));
+    }
+
+    @Test
+    //tests clear and size
+    public void TestLinkedList4() {
+        testList.clear();
+        assertEquals(0, testList.size());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
 }
